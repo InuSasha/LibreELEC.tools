c859fa8bef503d39f0abc6f6f32c21192f01a5fe
Sascha Kuehndel (InuSasha) <dev@inusasha.de>
2018-06-04 21:59:17 +0200

    kodi_sendnotification.sh - move options to ~/.libreelec - configurable message

--- kodi_sendnotification.sh
+++ kodi_sendnotification.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-. $(dirname $0)/options.txt
+. ~/.libreelec/options.tools
 
 title=$(hostname | tr '[:lower:]' '[:upper:]')
-msg="build done"
+if [ ! -z "$1" ]; then
+  msg="$1"
+else
+  msg="reminder from $(hostname)"
+fi
+
 json="{'jsonrpc': '2.0', 'method': 'GUI.ShowNotification', 'id': 1, 'params': {'title': '$title', 'message': '$msg' } }"
 curl -G http://$KODI_CLIENT/jsonrpc \
      --data-urlencode "request=${json//\'/\"}" \
